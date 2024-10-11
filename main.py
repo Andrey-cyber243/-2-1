d8d6734df29550940ad6e025d853868f68b85091
Andrey-cyber243 <Sapuntsov64@mail.ru>
2024-10-11 18:19:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-first_name='Курс:''Python'
-hwork_name=12# Количество выполненных ДЗ
-hours_name=1.5# Количество затраченных час
-time_name=(1.5/12)#  время выполнения в часах
-print (first_name, 'всего задач', hwork_name,'затрачено часов',hours_name,' время выполнения',time_name)
+
+print('Hi,PyCharm')
+x=43
+y=32
+print(x*y)
+print("End line")
