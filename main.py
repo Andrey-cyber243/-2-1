69275bb381d9f134da194bf4e029c67c45e2fa94
Andrey-cyber243 <Sapuntsov64@mail.ru>
2024-10-11 18:15:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+first_name='Курс:''Python'
+hwork_name=12# Количество выполненных ДЗ
+hours_name=1.5# Количество затраченных час
+time_name=(1.5/12)#  время выполнения в часах
+print (first_name, 'всего задач', hwork_name,'затрачено часов',hours_name,' время выполнения',time_name)
